d9fbde5ae2f4fc7886c1bf90a66c7385f1553b03
RafaelMorais-source <rmairinkmorais@gmail.com>
2020-11-14 14:27:54 -0300

    Aula.JavaScript

---
+++ js/main.js
@@ -0,0 +1,27 @@
+
+function clicou(){
+    document.getElementById("agradecimento").innerHTML="Obrigado por clicar";
+    //console.log(document.getElementById(agradecimento))
+    //alert("Obrigado por clicar");
+}
+
+/*function soma(n1, n2) {
+    return n1 + n2;
+}
+*/
+/*
+var lista = ["maça", "pêra", "laranja"];
+lista.push("uva");
+console.log(lista);
+*/
+/*
+var nome = "Rafael Morais";
+var idade = 32;
+var idade2 = 10;
+var frase = "Brasil é o melhor time do mundo"
+//alert( nome + " tem " + idade + " anos " );
+//alert(idade + idade2);
+console.log(nome);
+console.log(idade + idade2);
+console.log(frase);
+*/
